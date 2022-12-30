ebc2915923f14dac032c495d3f188748943dca95
Mikel Miras <mmiras004@ikasle.ehu.eus>
2022-12-30 23:34:37 +0100

    reduction

--- 3-proiektua/funtg_p.c
+++ 3-proiektua/funtg_p.c
@@ -93,7 +93,7 @@ float batuketa_totala = 0.0f;
 float kop;
   // Kalkulatu taldeen trinkotasuna: kideen arteko distantzien batezbestekoa
   // =======================================================================
-  #pragma omp parallel for private(kop, batuketa) shared(batuketa_totala)
+  #pragma omp parallel for private(kop, batuketa) shared(batuketa_totala) reduction(+:batuketa_totala)
   for ( int i = 0; i < taldekop; i++) {
 	batuketa_totala = 0;
 	kop = kideak[i].kop;
